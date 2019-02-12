559298b3e224f504f88eddd63ba68d0ed5df4bb9
Sangwon Jung <sangwon.jung@one-line.com>
2019-02-12 09:47:27 +0900

    방명록 틀 구성

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyD6cGW7r4WXJd3lJbd5VI0h3aRXKbpYofg",
+    authDomain: "jsw3070-gbook.firebaseapp.com",
+    databaseURL: "https://jsw3070-gbook.firebaseio.com",
+    projectId: "jsw3070-gbook",
+    storageBucket: "jsw3070-gbook.appspot.com",
+    messagingSenderId: "264570551322"
+  };
+  firebase.initializeApp(config);
